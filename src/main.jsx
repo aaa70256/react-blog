a7bf86dcd361541d621524b19687d5b2897ae383
peter <peter.t@webglsoft.com>
2024-11-11 17:17:37 +0800

    push to github pages v5

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/index.scss'
-import { RouterProvider, BrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { router } from './utils/router.jsx'
 import { UserProvider } from './utils/context/userContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserProvider>
-      <BrowserRouter basename="/react-blog">
-        <RouterProvider router={router} />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </UserProvider>
   </StrictMode>
 )
